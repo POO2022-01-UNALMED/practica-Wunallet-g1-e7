6881349cd99a675ac22950948f92be4a7f275c93
David Cardona Duque <davcardona@unal.edu.co>
2022-05-29 18:01:23 -0500

    Update Cuenta.java

--- Desarrollo/Wunallet/src/cLogica/Cuenta.java
+++ Desarrollo/Wunallet/src/cLogica/Cuenta.java
@@ -19,4 +19,46 @@ public abstract class Cuenta implements Gestor{
 		this.tipoDeCuenta = tipoDeCuenta;
 	}
 	
+//	-------------------------------------- Metodos get-set --------------------------------------
+
+	public void setNroCuenta(Cuenta nroCuenta) {
+		this.nroCuenta = nroCuenta;
+	}
+	public int getNroCuenta() {
+		return this.nroCuenta;
+	}
+	
+	public void setTitular(Usuario titular) {
+		this.titular = titular;
+	}
+	public Usuario getTitular() {
+		return this.titular;
+	}
+	
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+	public float getSaldo() {
+		return this.saldo;
+	}
+	
+	public void setBanco(Banco banco) {
+		this.banco = banco;
+	}
+	public Banco getBanco() {
+		return this.banco;
+	}
+	
+	public void setTipoCuenta(String tipoCuenta) {
+		this.tipoCuenta = tipoCuenta;
+	}
+	public String getTipoCuenta() {
+		return this.tipoCuenta;
+	}
+	
+	
+	//--------------------------------------------------------------------------------------
+
+	
+	
 }
