8a5fa94a46ecf395891c81b10994c7a188e58ae1
David Cardona Duque <davcardona@unal.edu.co>
2022-05-29 17:49:40 -0500

    Update Transaccion.java

--- Desarrollo/Wunallet/src/cLogica/Transaccion.java
+++ Desarrollo/Wunallet/src/cLogica/Transaccion.java
@@ -14,8 +14,39 @@ public class Transaccion {
 		
 	}
 	
+//	-------------------------------------- Metodos get-set --------------------------------------
+
+	public void setCuentaOrigen(Cuenta cuentaOrigen) {
+		this.cuentaOrigen = cuentaOrigen;
+	}
+	public Cuenta getCuentaOrigen() {
+		return this.cuentaOrigen;
+	}
+	
+	public void setCuentaDestino(Cuenta cuentaDestino) {
+		this.cuentaDestino = cuentaDestino;
+	}
+	public Cuenta getCuentaDestino() {
+		return this.cuentaDestino;
+	}
+	
+	public void setNombreBanco(String nombreBanco) {
+		this.nombreBanco = nombreBanco;
+	}
+	public float getNombreBanco() {
+		return this.nombreBanco;
+	}
+	
+	public void setValorTransaccion(float valorTransaccion) {
+		this.valorTransaccion = valorTransaccion;
+	}
+	public float getValorTransaccion() {
+		return this.valorTransaccion;
+	}
 	
 	
+	//--------------------------------------------------------------------------------------
+
 	public String toString() {
 		
 		if (this.cuentaDestino == NULL) {
