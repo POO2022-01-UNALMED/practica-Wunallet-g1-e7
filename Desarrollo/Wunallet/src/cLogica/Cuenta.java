98a283d0a18d9fe69b0b609a3e5137ac9151f8fc
David Cardona Duque <davcardona@unal.edu.co>
2022-05-29 20:42:45 -0500

    Update Cuenta.java

--- Desarrollo/Wunallet/src/cLogica/Cuenta.java
+++ Desarrollo/Wunallet/src/cLogica/Cuenta.java
@@ -66,4 +66,8 @@ public abstract class Cuenta implements Gestor{
 		}
 	}
 	
+	abstract boolean transferir(Cuenta cuentaDestino, float valorTranseferencia);
+	
+	abstract boolean transferir(Credito credito);
+
 }
