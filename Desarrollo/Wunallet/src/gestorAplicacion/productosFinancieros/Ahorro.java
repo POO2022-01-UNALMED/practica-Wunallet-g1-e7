48133798efb7550ec825427e5e3bad940667d7d0
David Cardona Duque <davcardona@unal.edu.co>
2022-05-30 18:28:40 -0500

    Update Ahorro.java

--- Desarrollo/Wunallet/src/gestorAplicacion/productosFinancieros/Ahorro.java
+++ Desarrollo/Wunallet/src/gestorAplicacion/productosFinancieros/Ahorro.java
@@ -4,21 +4,41 @@ import java.util.ArrayList;
 import gestorAplicacion.infoClientes.Banco;
 import gestorAplicacion.infoClientes.Transaccion;
 import gestorAplicacion.infoClientes.Usuario;
-import gestorAplicacion.productosFinancieros.Cuenta;
 
-public class Ahorro extends Cuenta{
+import gestorAplicacion.productosFinancieros.Cuenta;
+import java.io.Serializable;
+
+
+
+
+public class Ahorro extends Cuenta implements Serializable{
 	protected float tasaDeInteres;
+	
+    // El Array de clase de clientes de encarga de guardar todas las instancias de
+    // Cliente para poder guardar y cargarlas en la serializacion
+	private static ArrayList<Ahorro> ahorro = new ArrayList<>();
+
 	
 	public Ahorro(int nroCuenta, Usuario titular,float saldo,Banco banco,String tipoDeCuenta,ArrayList<Transaccion> historial,float tasaDeInteres) {
 		super(nroCuenta,titular,saldo,banco,tipoDeCuenta,historial);
 		this.tasaDeInteres = tasaDeInteres;
 		banco.getListaCuentas().add(this);
 		titular.getCuentasAsocidas().add(this);
+		
+		ahorro.add(this);
 	}
 
 
 //	-------------------------------------- Metodos get-set --------------------------------------
 //	get-set user
+    public static ArrayList<Ahorro> getAhorro() {
+        return ahorro;
+    }
+
+    public static void setClientes(ArrayList<Ahorro> ahorro) {
+        Ahorro.ahorro = ahorro;
+    }
+	
 	public void setTasaDeInteres(float tasaDeInteres) {
 		this.tasaDeInteres = tasaDeInteres;
 	}
